85a9ea3438f1a318d16a5761e8acfd788ad4b5c8
Lucyyannn <ML07220502@163.com>
2025-04-08 20:20:13 +0800

    edit typesystem

--- include/type.h
+++ include/type.h
@@ -1,8 +1,12 @@
 #ifndef TYPE_H
 #define TYPE_H
 
-enum class OpType {
-	// not defined 
+#include <iostream>
+#include <vector>
+class OpType{
+public:
+    enum Op{
+        // not defined 
 	Void,
 
     // arithmetic
@@ -24,15 +28,79 @@ enum class OpType {
     Gt,         // >
     Le,         // <=
     Ge          // >=
+    }optype;
+
+    OpType() { optype = Void; }
+    OpType(Op op) { optype = op; }
+    std::string GetOpTypeString();
 };
 
+
+// class Type {
+// public:
+//     // 我们认为数组的类型为PTR
+//     // 为了方便写浮点数指针，我们把PTR细化成整型指针和浮点数指针
+//     enum ty { VOID = 0, INT = 1, FLOAT = 2, BOOL = 3, INT_PTR = 4, FLOAT_PTR = 5, DOUBLE = 6 } type;
+//     std::string GetTypeInfo();
+//     Type() { type = VOID; }
+// };
+
 class Type {
-public:
-    // 我们认为数组的类型为PTR
-    // 为了方便写浮点数指针，我们把PTR细化成整型指针和浮点数指针
-    enum ty { VOID = 0, INT = 1, FLOAT = 2, BOOL = 3, INT_PTR = 4, FLOAT_PTR = 5, DOUBLE = 6 } type;
-    std::string GetTypeInfo();
-    Type() { type = VOID; }
+    public:
+        enum TypeKind {
+            Builtin,
+            Pointer,
+            Array
+        };
+    TypeKind kind;
+    virtual ~Type() = default;
 };
+    
+class BuiltinType : public Type {
+public:
+    enum BuiltinKind { Int, Float, String };
+    BuiltinKind builtinKind;
+    BuiltinType(BuiltinKind kind) : builtinKind(kind) {
+        this->kind = Type::Builtin;
+    }
+};
+    
+    class PointerType : public Type {
+    public:
+        Type* pointeeType;  // the type ptr point to 
+        PointerType(Type* pointee) : pointeeType(pointee) {
+            this->kind = Type::Pointer;
+        }
+    };
+    
+class ArrayType : public Type {
+public:
+    Type* elementType;
+    int length;         
+        
+    ArrayType(Type* elementType, int length)
+        : elementType(elementType), length(length) {
+        kind = Type::Array;
+    }
+    
+    bool isFixedSize() const { return length >= 0; }
+};
+
+class NodeAttribute {
+public:
+    int line_number = -1;
+    Type T;
+    bool ConstTag;
+    union ConstVal {
+        bool BoolVal;
+        int IntVal;
+        float FloatVal;
+        double DoubleVal;
+    } val;
+    NodeAttribute() { ConstTag = false; val.IntVal=0;}
+    std::string GetAttributeInfo();
+    std::string GetConstValueInfo(Type ty);
+};
+
 
 #endif
