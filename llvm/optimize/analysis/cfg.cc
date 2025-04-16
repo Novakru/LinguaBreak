571c59150ff1ec0b4721c1b1dd98f3185f547c20
Novakru <jiangjiahao0208@outlook.com>
2025-04-16 17:00:20 +0800

    framework build.

---
+++ llvm/optimize/analysis/cfg.cc
@@ -0,0 +1,30 @@
+#include "../../include/Instruction.h"
+#include "../../include/ir.h"
+#include <assert.h>
+
+void LLVMIR::CFGInit() {
+    for (auto &[defI, bb_map] : function_block_map) {
+        CFG *cfg = new CFG();
+        cfg->block_map = &bb_map;
+        cfg->function_def = defI;
+        cfg->BuildCFG();
+        TODO("init your members in class CFG if you need");
+        llvm_cfg[defI] = cfg;
+    }
+}
+
+void LLVMIR::BuildCFG() {
+    for (auto [defI, cfg] : llvm_cfg) {
+        cfg->BuildCFG();
+    }
+}
+
+void CFG::BuildCFG() { TODO("BuildCFG"); }
+
+std::vector<LLVMBlock> CFG::GetPredecessor(LLVMBlock B) { return invG[B->block_id]; }
+
+std::vector<LLVMBlock> CFG::GetPredecessor(int bbid) { return invG[bbid]; }
+
+std::vector<LLVMBlock> CFG::GetSuccessor(LLVMBlock B) { return G[B->block_id]; }
+
+std::vector<LLVMBlock> CFG::GetSuccessor(int bbid) { return G[bbid]; }
